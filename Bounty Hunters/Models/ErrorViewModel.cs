c67e4caf5c6b56ec6284a70c85bd1a442032761c
Ina <inatkata@yahoo.com>
2024-11-12 07:49:29 +0000

    Add project files.

---
+++ Bounty Hunters/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bounty_Hunters.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
