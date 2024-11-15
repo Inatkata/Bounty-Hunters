6241e6628ceedc116238e597a619979b764bcc9b
Ina <inatkata@yahoo.com>
2024-11-15 15:33:14 +0000

    AccountController Creation

---
+++ Bounty Hunters/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bounty_Hunters.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
