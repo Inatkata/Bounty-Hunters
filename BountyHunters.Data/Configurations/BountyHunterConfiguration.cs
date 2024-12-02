22ce7921875adddca24c0caa187093ee1699e472
Ina <inatkata@yahoo.com>
2024-12-02 14:23:07 +0100

    BountyHunters Config builders

--- BountyHunters.Data/Configurations/BountyHunterConfiguration.cs
+++ BountyHunters.Data/Configurations/BountyHunterConfiguration.cs
@@ -13,11 +13,67 @@ namespace BountyHunters.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<BountyHunter> builder)
         {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(b => b.Rank)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(b => b.Bio)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(b => b.CaptureCount)
+                .IsRequired()
+                .HasDefaultValue(0);
+
+
             builder.HasMany(b => b.Captures)
                 .WithOne(c => c.BountyHunter)
                 .HasForeignKey(c => c.BountyHunterId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            builder.HasData(this.SeedBountyHunters());
+        }
+
+        private IEnumerable<BountyHunter> SeedBountyHunters()
+        {
+            IEnumerable<BountyHunter> bountyHunters = new List<BountyHunter>()
+            {
+                new BountyHunter
+                {
+                    Name = "John Tracker",
+                    Rank = "Novice",
+                    Bio = "A new bounty hunter ready for action.",
+                    CaptureCount = 0
+                },
+                new BountyHunter
+                {
+                    Name = "Jane Swift",
+                    Rank = "Expert",
+                    Bio = "Experienced bounty hunter with countless captures.",
+                    CaptureCount = 8
+                },
+                new BountyHunter
+                {
+                    Name = "Alice Tracker",
+                    Rank = "Novice",
+                    Bio = "New bounty hunter",
+                    CaptureCount = 0
+                },
+                new BountyHunter
+                {
+                    Name = "Bob Hunter",
+                    Rank = "Expert",
+                    Bio = "Seasoned hunter.",
+                    CaptureCount = 10
+                }
+            };
+            return bountyHunters;
         }
     }
 }
